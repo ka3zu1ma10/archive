5fad89d90c7befa3b60c40a8357cd4868b6edb69
西藤和馬 <saitoukazuma@saitoukazumanoiMac.local>
2019-07-29 18:45:10 +0900

    phptest

--- index-old.php
+++ index-old.php
@@ -1,56 +1,76 @@
 <!DOCTYPE html>
-<html lang="jp">
-  <head>
-    <meta charset="utf-8">
-    <title>Kazuma Saitoh Info Card</title>
-    <link rel="stylesheet" href="css/master.css">
-    <link rel="stylesheet" href="css/ress.css">
-    <link href="https://fonts.googleapis.com/css?family=Rajdhani" rel="stylesheet">
-  </head>
-  <body>
-    <main class="main-body">
-      <h1>Kazuma Saitoh<br>Information</h1>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>Kazuma Saitoh</title>
+  <link rel="stylesheet" href="./css/ress.css">
+  <link rel="stylesheet" href="./css/master.css">
+  <link rel="stylesheet" href="./css/lightbox.css">
+  <title></title>
+</head>
+
+<body>
+  <header id="main-header">
+    <h1>Kazuma Saitoh</h1>
+  </header>
+  <main id="main-area">
+    <menu id="main-menu">
+      <ul class="menu-lists">
+        <li><button class="menu-list_button menu-list_button__active" type="button" name="button">WELLCOM</button></li>
+        <li><button class="menu-list_button" type="button" name="button">PHOTOs</button></li>
+        <li><button class="menu-list_button" type="button" name="button">DESIGNs</button></li>
+        <li><button class="menu-list_button" type="button" name="button">ABOUT ME</button></li>
+      </ul>
+    </menu>
+    <article id="main-body">
+      <h2>WELLCOM<br>Kazuma Saitoh Pages</h2>
       <section>
-        <h2>Are you sure you want to follow me</h2>
+        <h3>Are you sure you want to follow me</h3>
         <ul class="follow-list">
           <li class="button follow-twitter">
             <a href="https://twitter.com/ka3zu1ma10?ref_src=twsrc%5Etfw" data-show-count="false">
-              <img class="twitter-icon" src="images/Twitter_Logo_WhiteOnImage.svg"><p>Twitter Follow<br>@ka3zu1ma10</p>
+              <img class="twitter-icon" src="images/Twitter_Logo_WhiteOnImage.svg">
+              <p>Twitter Follow<br>@ka3zu1ma10</p>
             </a>
-            </li>
-            <li class="button follow-facebook">
-              <a href="https://www.facebook.com/ka3zu1ma10">
-                <img class="facebook-icon" src="images/flogo-HexRBG-Wht-58.svg">
-                <p>Facebook Follow<br>ka3zu1ma10</p>
-              </a>
-            </li>
+          </li>
+          <li class="button follow-facebook">
+            <a href="https://www.facebook.com/ka3zu1ma10">
+              <img class="facebook-icon" src="images/flogo-HexRBG-Wht-58.svg">
+              <p>Facebook Follow<br>ka3zu1ma10</p>
+            </a>
+          </li>
         </ul>
       </section>
       <section>
         <h2>My Photolist</h2>
         <ul>
           <?php
-          // ディレクトリハンドルの取得
-          $dir_h = opendir( "images/img-list/" ) ;
-          // ファイル・ディレクトリの一覧を $file_list に
-          while (false !== ($file_list[] = readdir($dir_h))) ;
-          // ディレクトリハンドルを閉じる
-          closedir( $dir_h ) ;
-          //ディレクトリ内のファイル名を１つずつを取得
-          foreach ( $file_list as $file_name )
-          {
-            //ファイルのみを表示
-            if( is_file( "images/img-list/" . $file_name) )
-            {
-              $p = pathinfo("images/img-list/" . $file_name);
-              if ( $p["extension"] == "JPG" )
-              {
-                print $file_name  ;
+          foreach(glob('./images/*') as $file){
+            if(is_file($file)){
+              echo htmlspecialchars($file);
+              $imagedir = opendir("./images/top-image/");
+              while (false !== ($file[] = readdir($imagedir)));
+              closedir($imagedir);
+              natsort($file);
+              reset($file);
+              $reverse = array_reverse($file, true);
+              while (false !== ($jpg = each($reverse))){
+              if (preg_match ("|.JPG$|", $jpg[1])) {
+                print "<li><a href='./images/top-image/" . $jpg[1] . "' data-lightbox='image-1'><img class='images' src='./images/top-image/" . $jpg[1] . "'></a></li>";
               }
             }
-          }?>
+          }
+        }
+
+          ?>
         </ul>
       </section>
-    </main>
-  </body>
+    </article>
+  </main>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="./js/lightbox.js"></script>
+  <script src="./js/main.js"></script>
+</body>
+
 </html>
