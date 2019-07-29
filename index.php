045a8bb66bcf78fb175ce0d60a173d64cbadbb01
西藤和馬 <saitoukazuma@saitoukazumanoiMac.local>
2019-07-29 22:00:14 +0900

    test

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-
+<!DOCTYPE html>
 <html lang="ja">
 
 <head>
@@ -19,30 +19,12 @@
       <ul class="menu-lists">
         <li><a class="menu-list_button menu-list_button__active" href="#">WELLCOM</a></li>
         <li><a class="menu-list_button" href="#">PHOTOs</a></li>
-        <li><a class="menu-list_button" href="photos.php" >DESIGNs</a></li>
+        <li><a class="menu-list_button" href="photos.php">DESIGNs</a></li>
         <li><a class="menu-list_button" href="aboutme.html">ABOUT ME</a></li>
       </ul>
     </menu>
     <article id="main-body">
-      <section id="photos" class="main-body--noneactiv">
-        <h2>My Photolist</h2>
-        <h3>mitaka</h3>
-        <ul class="photos-photolists">
-          <?php
-          $imagedir = opendir("./images/img-list/mitaka-20190616/");
-          while (false !== ($file[] = readdir($imagedir)));
-          closedir($imagedir);
-          natsort($file);
-          reset($file);
-          $reverse = array_reverse($file, true);
-          while (false !== ($jpg = each($reverse))){
-          if (preg_match ("|.jpg$|", $jpg[1])) {
-            print "<li class='photos-photolist'><a href='./images/img-list/mitaka-20190616/" . $jpg[1] . "' data-lightbox='image-1'><img class='images' src='./images/img-list/mitaka-20190616/" . $jpg[1] . "'></a></li>";
-          }
-          }
-          ?>
-        </ul>
-      </section>
+      <?php include ('wellcom.html'); ?>
     </article>
   </main>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
