7111ce9b00bc9a136112d210157969e22963b6f6
西藤和馬 <saitoukazuma@saitoukazumanoiMac.local>
2019-10-06 07:41:15 +0900

    cssの調整

--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@ lightbox.option({
   disableScrolling: true
 })
 
-// masthead scroll
-var header = $('#masthead'); // fixed DOM
-var adclass = 'scrolled'; // add css class
-var scrollY = 90; // scroll
+// // masthead scroll
+// var header = $('#masthead'); // fixed DOM
+// var adclass = 'scrolled'; // add css class
+// var scrollY = 90; // scroll
 
-$(window).scroll(function() {
-  if ($(window).scrollTop() > scrollY) {
-    header.addClass(adclass);
-  } else {
-    header.removeClass(adclass);
-  }
-});
+// $(window).scroll(function() {
+//   if ($(window).scrollTop() > scrollY) {
+//     header.addClass(adclass);
+//   } else {
+//     header.removeClass(adclass);
+//   }
+// });
 
 var getMainMenuEls = document.getElementsByClassName('menu-list_button');
 
