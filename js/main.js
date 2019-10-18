da5e8aa5fa3173ce1173bacbee84f6c2f7ff9092
西藤和馬 <saitoukazuma@saitoukazumanoiMac.local>
2019-10-19 02:46:56 +0900

    js IE対応

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ const getMainMenuEls = document.getElementsByClassName('menu-list_button');
 
 const clickMainMenuHandler = function(resp) {
   let getMainBodyEls = document.getElementsByClassName('main-body-date--active');
-  for (let getMainMenuEl of getMainMenuEls) {
-    getMainMenuEl.classList.remove('menu-list_button__active');
+  for (let j = 0; j < getMainMenuEls.length; j++) {
+    getMainMenuEls[j].classList.remove('menu-list_button__active');
   }
   getMainBodyEls[0].classList.remove('main-body-date--active');
   resp.target.classList.add('menu-list_button__active');
@@ -17,17 +17,17 @@ const clickMainMenuHandler = function(resp) {
   getActivBodyEl.classList.add('main-body-date--active');
 };
 
-for (let getMainMenuEl of getMainMenuEls) {
-  getMainMenuEl.addEventListener('click', clickMainMenuHandler);
+for (let k = 0; k < getMainMenuEls.length; k++) {
+  getMainMenuEls[k].addEventListener('click', clickMainMenuHandler);
 };
 
-const skillSetCreat = function(){
+const skillSetCreat = function() {
   const skillSetEl = document.querySelectorAll('.skill-set');
-  for (let skill in skillSet){
+  for (let l = 0; l < skillSet.length; l++) {
     let skillSetLi = document.createElement('li');
     skillSetLi.classList.add('skill-set_list');
     let skillSetP = document.createElement('p');
-    skillSetP.textContent = skill;
+    skillSetP.textContent = skillSet[l];
     skillSetLi.appendChild(skillSetP);
 
     // let skillSetImg = document.createElement('img');
@@ -36,12 +36,12 @@ const skillSetCreat = function(){
     // skillSetLi.appendChild(skillSetImg);
 
     let skillSetLevels = document.createElement('ul');
-    for (let i = 0; i < 5; i++){
+    for (let i = 0; i < 5; i++) {
       let skillSetLevel = document.createElement('li');
-      if (i < skillSet[skill].Level){
-        skillSetLevel.classList.add('fas','fa-star','skill-level_star-on');
-      }else{
-        skillSetLevel.classList.add('fas','fa-star','skill-level_star-off');
+      if (i < skillSet[skillSet[l]].Level) {
+        skillSetLevel.classList.add('fas', 'fa-star', 'skill-level_star-on');
+      } else {
+        skillSetLevel.classList.add('fas', 'fa-star', 'skill-level_star-off');
       }
       skillSetLevels.appendChild(skillSetLevel);
     }
