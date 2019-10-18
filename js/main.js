1847ad789c651add5069dbfe2074849079a76c56
西藤和馬 <saitoukazuma@saitoukazumanoiMac.local>
2019-10-19 03:38:18 +0900

    js IE対応2

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ const skillSetCreat = function() {
     let skillSetLevels = document.createElement('ul');
     for (let i = 0; i < 5; i++) {
       let skillSetLevel = document.createElement('li');
-      if (i < skillSet[skillSet[l]].Level) {
+      if (i < skillSet[l].Level) {
         skillSetLevel.classList.add('fas', 'fa-star', 'skill-level_star-on');
       } else {
         skillSetLevel.classList.add('fas', 'fa-star', 'skill-level_star-off');
