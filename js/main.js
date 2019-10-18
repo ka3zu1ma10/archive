f5bb5f2af15bff9b82c2dc6eba74873b63773141
西藤和馬 <saitoukazuma@saitoukazumanoiMac.local>
2019-10-19 03:05:53 +0900

    js IE対応2

--- js/main.js
+++ js/main.js
@@ -17,28 +17,23 @@ const clickMainMenuHandler = function(resp) {
   getActivBodyEl.classList.add('main-body-date--active');
 };
 
-for (let getMainMenuEl of getMainMenuEls) {
-  getMainMenuEl.addEventListener('click', clickMainMenuHandler);
+for (let k = 0; k < getMainMenuEls.length; k++) {
+  getMainMenuEls[k].addEventListener('click', clickMainMenuHandler);
 }
 
 const skillSetCreat = function() {
   const skillSetEl = document.querySelectorAll('.skill-set');
-  for (let skill in skillSet) {
+  for (let l = 0; k < skillSet.length; l++) {
     let skillSetLi = document.createElement('li');
     skillSetLi.classList.add('skill-set_list');
     let skillSetP = document.createElement('p');
     skillSetP.textContent = skillSet[l];
     skillSetLi.appendChild(skillSetP);
 
-    // let skillSetImg = document.createElement('img');
-    // skillSetImg.setAttribute('src',skill.icon);
-    // skillSetImg.classList.add('skill-set_icon');
-    // skillSetLi.appendChild(skillSetImg);
-
     let skillSetLevels = document.createElement('ul');
     for (let i = 0; i < 5; i++) {
       let skillSetLevel = document.createElement('li');
-      if (i < skillSet[skill].Level) {
+      if (i < skillSet[skillSet[l]].Level) {
         skillSetLevel.classList.add('fas', 'fa-star', 'skill-level_star-on');
       } else {
         skillSetLevel.classList.add('fas', 'fa-star', 'skill-level_star-off');
